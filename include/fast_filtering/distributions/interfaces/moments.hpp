3a9119d03a801d55836e5fe69b3f651a64f526ae
issac <jan.issac@gmail.com>
2014-11-07 15:21:16 +0100

    Added moments interface docs

--- include/fast_filtering/distributions/interfaces/moments.hpp
+++ include/fast_filtering/distributions/interfaces/moments.hpp
@@ -6,7 +6,7 @@
  *    Manuel Wuthrich (manuel.wuthrich@gmail.com)
  *    Jan Issac (jan.issac@gmail.com)
  *
- *  All rights reserved.
+ *
  *
  *  Redistribution and use in source and binary forms, with or without
  *  modification, are permitted provided that the following conditions
@@ -52,24 +52,54 @@
 namespace ff
 {
 
+/**
+ * \interface Moments
+ * \brief Moments is the interface providing the first two moments
+ *
+ * \tparam Vector   Random variable type. This is equivalent to the first moment
+ *                  type.
+ * \tparam Operator Second moment type
+ *
+ * The Moments interface provides access to the exact first moments of
+ * a distribution. The moments represent a subset of the approximate moments.
+ */
 template <typename Vector, typename Operator>
 class Moments:
-        public ApproximateMoments<Vector, Operator>
+    public ApproximateMoments<Vector, Operator>
 {
-public:
-    virtual ~Moments() {}
+public:    
 
+    /**
+     * \return First moment of the underlying distribution, the mean
+     */
     virtual Vector Mean() const = 0;
+
+    /**
+     * \return Second centered moment of the underlying distribution,
+     *         the covariance
+     */
     virtual Operator Covariance() const = 0;
 
-    virtual Vector   ApproximateMean()
+    /**
+     * \{ApproximateMoments::ApproximateMean() const\}
+     */
+    virtual Vector ApproximateMean() const
     {
         return Mean();
     }
-    virtual Operator ApproximateCovariance()
+
+    /**
+     * \{ApproximateMoments::ApproximateCovariance() const\}
+     */
+    virtual Operator ApproximateCovariance() const
     {
         return Covariance();
     }
+
+    /**
+     * \brief Overridable default destructor
+     */
+    virtual ~Moments() { }
 };
 
 }
