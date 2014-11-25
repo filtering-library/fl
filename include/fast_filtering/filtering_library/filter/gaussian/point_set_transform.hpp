3ad2894d84a01f6a4664a1a233119dbfda375ff8
issac <jan.issac@gmail.com>
2014-11-25 18:18:49 +0100

    Updated point set transform interface

--- include/fast_filtering/filtering_library/filter/gaussian/point_set_transform.hpp
+++ include/fast_filtering/filtering_library/filter/gaussian/point_set_transform.hpp
@@ -40,7 +40,6 @@
 /**
  * @date 11/5/2014
  * @author Jan Issac (jan.issac@gmail.com)
- * @author Manuel Wuthrich (manuel.wuthrich@gmail.com)
  * Max-Planck-Institute for Intelligent Systems,
  * University of Southern California
  */
@@ -49,6 +48,9 @@
 #define FL__FILTER__GAUSSIAN__POINT_SET_TRANSFORM_HPP
 
 #include <cstddef>
+#include <fast_filtering/utils/meta.hpp>
+#include <fast_filtering/distributions/gaussian.hpp>
+#include <fast_filtering/filtering_library/filter/gaussian/point_set_gaussian.hpp>
 
 namespace fl
 {
@@ -60,15 +62,59 @@ namespace fl
  * is the well known Unscented Transform \cite wan2000unscented.
  *
  *
- * \tparam PointSetGaussian_    Gaussian distribution with point set
- *                              representation capability.
- * \tparam Gaussian_            Source Gaussian distribution type
+ * \tparam Derived  PointSetTransform implementation type which must provide the
+ *                  interface stated below.
  */
-template <typename PointSetGaussian_, typename Gaussian_ = PointSetGaussian_>
+template <typename Derived>
 class PointSetTransform
 {
 public:
     /**
+     * Asserts correct interface
+     *
+     * @param derived instance pointer
+     */
+    explicit PointSetTransform(const Derived* const derived)
+    {
+        typedef Eigen::Matrix<double, 1, 1> Point;
+
+        /** \cond INTERNAL */
+        /** \remarks */
+
+        /**
+         * - Assert the existence of the function
+         * \code constexpr size_t number_of_points(int dimension) const \endcode
+         */
+        PointSetGaussian<Point, Derived::number_of_points(1)> point_set;
+
+        /**
+         * - Asserts the existens of the forward function with the required
+         * parameters \code
+         *   void forward(const Gaussian& src, PointSetGaussian& dest) const
+         * \endcode
+         */
+        //derived->forward(ff::Gaussian<Point>(), point_set);
+
+        /**
+         * - Asserts the existens of the forward function with the required
+         * parameters \code
+         *   void forward(const Gaussian& src,
+         *                size_t global_dimension,
+         *                size_t dimension_offset,
+         *                PointSetGaussian& dest) const
+         * \endcode
+         */
+        //derived->forward(ff::Gaussian<Point>(), 1, 0, point_set);
+
+        /** \endcond */
+    }
+
+#ifdef PARSED_BY_DOXYGEN
+    /**
+     * \note This function is not explicitly definded nor implemented within
+     *       PointSetTransform. However, it is implicitly required to be
+     *       implemented in the Derived class.
+     *
      * Transforms the given Gaussian into a statistic represented as a set of
      * points.
      *
@@ -77,22 +123,19 @@ public:
      *                          selected set of points represeting the source
      *                          Gaussian.
      */
-    virtual void forward(const Gaussian_& gaussian,
-                         PointSetGaussian_& transform) const = 0;
+    template <typename Gaussian, typename PointSetGaussian>
+    void forward(const Gaussian& gaussian,
+                 PointSetGaussian& transform) const;
 
     /**
-     * @param [in]  gaussian        Source Gaussian distribution
-     * @param [in]  global_rank     Rank of the global covariance
-     * @param [in]  rank_offset     Rank offset determining the position of the
-     *                              current marginal Gaussian.
-     * @param [out] transform       Destination PointSetGaussian containing the
-     *                              selected set of points represeting the
-     *                              source Gaussian.
+     * \note This function is not explicitly definded nor implemented within
+     *       PointSetTransform. However, it is implicitly required to be
+     *       implemented in the Derived class.
      *
-     * Performs a local transform on a partiton of a Gaussian.
+     * Performs a local transform on a partition of a Gaussian.
      * The given marginal Gaussian is transformed into a statistic represented
-     * as a set of points \f${\cal X}_i\f$. The given Gaussian is assumed to be a
-     * marginal Gaussian of a larger joint Gaussian with a block diagonal
+     * as a set of points \f${\cal X}_i\f$. The given Gaussian is assumed to be
+     * a marginal Gaussian of a larger joint Gaussian with a block diagonal
      * covariance matrix
      *
      * \f$
@@ -104,7 +147,7 @@ public:
      * \f$
      * where \c gaussian = \f$\Sigma_i\f$.
      *
-     * The number points in \f${\cal X}\f$ is a function of \f$rank(\Sigma_i)\f$.
+     * The number points in \f${\cal X}\f$ is a function of\f$dim(\Sigma_i)\f$.
      * Let the full transform of \f$\Sigma\f$ be
      *
      * \f$
@@ -121,11 +164,21 @@ public:
      * \note Note that performing a regular transform on
      * \f$\Sigma_i\f$ as the full covariance will result in a different and
      * a smaller set of points \f${\cal X}'_i\f$ with different \em weights.
+     *
+     * @param [in]  gaussian         Source Gaussian distribution
+     * @param [in]  global_dimension Dimension of the global covariance
+     * @param [in]  dimension_offset Dimension offset determining the position
+     *                               of the current marginal Gaussian.
+     * @param [out] transform        Destination PointSetGaussian containing the
+     *                               selected set of points represeting the
+     *                               source Gaussian.
      */
-    virtual void forward(const Gaussian_& gaussian,
-                         size_t global_rank,
-                         size_t rank_offset,
-                         PointSetGaussian_& transform) const = 0;
+    template <typename Gaussian, typename PointSetGaussian>
+    void forward(const Gaussian& gaussian,
+                 size_t global_dimension,
+                 size_t dimension_offset,
+                 PointSetGaussian& transform) const;
+#endif
 };
 
 }
