f47a5f05246e5d21e811ef0e3379091dfaca094b
issac <jan.issac@gmail.com>
2014-12-10 15:02:00 +0100

    Added doxygen layout, config and theme

---
+++ doc/theme/navtree_expand_selected.js
@@ -0,0 +1,23 @@
+function selectAndHighlight(hash,n)
+{  
+  var a;
+  if (hash) {
+    var link=stripPath($(location).attr('pathname'))+':'+hash.substring(1);
+    a=$('.item a[class$="'+link+'"]');
+  }
+  if (a && a.length) {
+    a.parent().parent().addClass('selected');
+    a.parent().parent().attr('id','selected');
+    highlightAnchor();
+  } else if (n) {
+    $(n.itemDiv).addClass('selected');
+    $(n.itemDiv).attr('id','selected');
+  }
+  if ($('#nav-tree-contents .item:first').hasClass('selected')) {
+    $('#nav-sync').css('top','30px');
+  } else {
+    $('#nav-sync').css('top','5px');
+  }   
+  expandNode(new Object(), n, true, true);
+  showRoot();  
+};
