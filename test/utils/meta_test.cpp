deb8dd0eff94bd7d019aa9fc1c74b96836400b3c
issac <jan.issac@gmail.com>
2015-01-29 14:12:23 +0100

    Added meta unit tests

---
+++ test/utils/meta_test.cpp
@@ -0,0 +1,74 @@
+/*
+ * This is part of the FL library, a C++ Bayesian filtering library
+ * (https://github.com/filtering-library)
+ *
+ * Copyright (c) 2014 Jan Issac (jan.issac@gmail.com)
+ * Copyright (c) 2014 Manuel Wuthrich (manuel.wuthrich@gmail.com)
+ *
+ * Max-Planck Institute for Intelligent Systems, AMD Lab
+ * University of Southern California, CLMC Lab
+ *
+ * This Source Code Form is subject to the terms of the MIT License (MIT).
+ * A copy of the license can be found in the LICENSE file distributed with this
+ * source code.
+ */
+
+/**
+ * \file meta_test.hpp
+ * \date Januray 2015
+ * \author Jan Issac (jan.issac@gmail.com)
+ */
+
+#include <gtest/gtest.h>
+
+#include <Eigen/Dense>
+
+#include <fl/util/math.hpp>
+#include <fl/util/traits.hpp>
+#include <fl/util/meta.hpp>
+
+#include <cmath>
+
+TEST(MetaTests, FactorSize)
+{
+    EXPECT_EQ((fl::FactorSize<-1, 10>::Size), -1);
+    EXPECT_EQ((fl::FactorSize<10, -1>::Size), -1);
+    EXPECT_EQ((fl::FactorSize<-1, -1>::Size), -1);
+
+    EXPECT_EQ((fl::FactorSize<1, 1>::Size), 1);
+    EXPECT_EQ((fl::FactorSize<1, 10>::Size), 10);
+
+    EXPECT_EQ((fl::FactorSize<2, 10>::Size), 20);
+    EXPECT_EQ((fl::FactorSize<3, 5>::Size), 15);
+}
+
+
+TEST(MetaTests, JoinSizes_single_param)
+{
+    EXPECT_EQ((fl::JoinSizes<0>::Size), 0);
+    EXPECT_EQ((fl::JoinSizes<-1>::Size), -1);
+    EXPECT_EQ((fl::JoinSizes<1>::Size), 1);
+    EXPECT_EQ((fl::JoinSizes<2>::Size), 2);
+}
+
+TEST(MetaTests, JoinSizes_two_param)
+{
+    EXPECT_EQ((fl::JoinSizes<-1, -1>::Size), -1);
+    EXPECT_EQ((fl::JoinSizes<-1,  1>::Size), -1);
+    EXPECT_EQ((fl::JoinSizes< 1, -1>::Size), -1);
+    EXPECT_EQ((fl::JoinSizes< 1,  1>::Size),  2);
+
+    // EXPECT_EQ((fl::JoinSizes<-1,  0>::Size), -1); // should be rejected
+    // EXPECT_EQ((fl::JoinSizes< 1,  0>::Size),  2); // should be rejected
+}
+
+TEST(MetaTests, JoinSizes_multiple_param)
+{
+    EXPECT_EQ((fl::JoinSizes<-1, -1, 1, 2, -1>::Size), -1);
+    EXPECT_EQ((fl::JoinSizes< 1, -1, 1, 2, -1>::Size), -1);
+    EXPECT_EQ((fl::JoinSizes< 1,  1, 1, 2, -1>::Size), -1);
+    EXPECT_EQ((fl::JoinSizes<-1,  1, 1, 2, -1>::Size), -1);
+    EXPECT_EQ((fl::JoinSizes< 1, -1, 1, 2, -1>::Size), -1);
+    EXPECT_EQ((fl::JoinSizes<1, 1, 1, 1, 1>::Size), 5);
+    EXPECT_EQ((fl::JoinSizes<1, 2, 3, 4, 5>::Size), 5*6/2);
+}
