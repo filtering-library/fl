40ab1529b9ded0def888d5ed6a8dd701ea12a1b0
issac <jan.issac@gmail.com>
2015-02-13 15:05:39 +0100

    Added joint_process_model which conatins all implementations of JointProcessModel

---
+++ include/fl/model/process/joint_process_model.hpp
@@ -0,0 +1,28 @@
+/*
+ * This is part of the FL library, a C++ Bayesian filtering library
+ * (https://github.com/filtering-library)
+ *
+ * Copyright (c) 2014 Jan Issac (jan.issac@gmail.com)
+ * Copyright (c) 2014 Manuel Wuthrich (manuel.wuthrich@gmail.com)
+ *
+ * Max-Planck Institute for Intelligent Systems, AMD Lab
+ * University of Southern California, CLMC Lab
+ *
+ * This Source Code Form is subject to the terms of the MIT License (MIT).
+ * A copy of the license can be found in the LICENSE file distributed with this
+ * source code.
+ */
+
+/**
+ * \file joint_process_model.hpp
+ * \date Febuary 2015
+ * \author Jan Issac (jan.issac@gmail.com)
+ */
+
+#ifndef FL__MODEL__PROCESS__JOINT_PROCESS_MODEL_HPP
+#define FL__MODEL__PROCESS__JOINT_PROCESS_MODEL_HPP
+
+#include <fl/model/process/factorized_id_process_model.hpp>
+#include <fl/model/process/factorized_iid_process_model.hpp>
+
+#endif
