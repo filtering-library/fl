e77c085a0b17fe4cb33132116c09fe4454f45c3e
issac <jan.issac@gmail.com>
2015-02-16 17:39:07 +0100

    Implemented JointDistribution<MultipleOf<Distribution, Count>> for identically independently distributed variates

---
+++ include/fl/distribution/joint_distribution_iid.hpp
@@ -0,0 +1,147 @@
+/*
+ * This is part of the FL library, a C++ Bayesian filtering library
+ * (https://github.com/filtering-library)
+ *
+ * Copyright (c) 2014 Jan Issac (jan.issac@gmail.com)
+ * Copyright (c) 2014 Manuel Wuthrich (manuel.wuthrich@gmail.com)
+ *
+ * Max-Planck Institute for Intelligent Systems, AMD Lab
+ * University of Southern California, CLMC Lab
+ *
+ * This Source Code Form is subject to the terms of the MIT License (MIT).
+ * A copy of the license can be found in the LICENSE file distributed with this
+ * source code.
+ */
+
+/**
+ * \file joint_distribution_iid.hpp
+ * \date Febuary 2015
+ * \author Jan Issac (jan.issac@gmail.com)
+ */
+
+#ifndef FL__DISTRIBUTION__JOINT_DISTRIBUTION_IID_HPP
+#define FL__DISTRIBUTION__JOINT_DISTRIBUTION_IID_HPP
+
+#include <Eigen/Dense>
+
+#include <fl/util/meta.hpp>
+#include <fl/util/traits.hpp>
+
+#include <fl/distribution/interface/moments.hpp>
+
+namespace fl
+{
+
+// Forward declaration
+template <typename...Distributions> class JointDistribution;
+
+/**
+ * Traits of JointDistribution<Distribution, Count>
+ */
+template <typename Distribution, int Count>
+struct Traits<
+           JointDistribution<MultipleOf<Distribution, Count>>
+       >
+{
+    typedef typename Traits<Distribution>::Variate MarginalVariate;
+
+    enum : signed int
+    {
+        MarginalCount = sizeof...(Distribution),
+        Dimension = FactorSizes<MarginalVariate::SizeAtCompileTime, Count>::Size
+    };
+
+    typedef typename MarginalVariate::Scalar Scalar;
+
+    typedef Eigen::Matrix<Scalar, Dimension, 1> Variate;
+    typedef Eigen::Matrix<Scalar, Dimension, Dimension> SecondMoment;
+    typedef Eigen::Matrix<Distribution, Count, 1> MarginalDistributions;
+
+    typedef Moments<Variate, SecondMoment> MomentsInterface;
+};
+
+/**
+ * \ingroup distributions
+ */
+template <typename MarginalDistribution, int Count>
+class JointDistribution<MultipleOf<MarginalDistribution, Count>>
+    : public Traits<
+                 JointDistribution<
+                     MultipleOf<MarginalDistribution, Count>
+                 >
+             >::MomentsInterface
+{
+public:
+    typedef JointDistribution<MultipleOf<MarginalDistribution, Count>> This;
+
+    typedef typename Traits<This>::Variate Variate;
+    typedef typename Traits<This>::SecondMoment SecondMoment;
+    typedef typename Traits<This>::MarginalDistributions MarginalDistributions;
+
+public:
+    JointDistribution(MarginalDistributions distributions)
+        : distributions_(distributions)
+    {
+        for (int i = 0; i < distributions_.rows(); ++i)
+        {
+            dimension_ += distributions_(i).dimension();
+        }
+    }
+
+    /**
+     * \brief Overridable default destructor
+     */
+    virtual ~JointDistribution() { }
+
+    virtual Variate mean() const
+    {
+        Variate mu = Variate(dimension(), 1);
+
+        int offset = 0;
+        for (int i = 0; i < Traits<This>::MarginalCount; ++i)
+        {
+            MarginalDistribution& marginal = distributions_(i);
+            int dim =  marginal.dimension();
+
+            mu.middleRows(offset, dim) = marginal.mean();
+
+            offset += dim;
+        }
+
+        return mu;
+    }
+
+    virtual SecondMoment covariance() const
+    {
+        SecondMoment cov = SecondMoment::Zero(dimension(), dimension());
+
+        int offset = 0;
+        for (int i = 0; i < distributions_.rows(); ++i)
+        {
+            MarginalDistribution& marginal = distributions_(i);
+            int dim =  marginal.dimension();
+
+            cov.block(offset, offset, dim, dim) = marginal.covariance();
+
+            offset += dim;
+        }
+
+        return cov;
+    }
+
+    virtual int dimension() const
+    {
+        return dimension_;
+    }
+
+    MarginalDistributions& distributions()
+    {
+        return distributions_;
+    }
+
+protected:
+    MarginalDistributions distributions_;
+    int dimension_;
+};
+
+}
