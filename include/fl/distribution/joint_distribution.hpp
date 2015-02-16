ab4244fd198d191f2c6ec3b60b6bf6fee7509710
issac <jan.issac@gmail.com>
2015-02-16 17:39:34 +0100

    Added joint_distribution which includes both joint_distribution_{id,idd} implementations

---
+++ include/fl/distribution/joint_distribution.hpp
@@ -0,0 +1,28 @@
+/*
+ * This is part of the FL library, a C++ Bayesian filtering library
+ * (https://github.com/filtering-library)
+ *
+ * Copyright (c) 2014 Jan Issac (jan.issac@gmail.com)
+ * Copyright (c) 2014 Manuel Wuthrich (manuel.wuthrich@gmail.com)
+ *
+ * Max-Planck Institute for Intelligent Systems, AMD Lab
+ * University of Southern California, CLMC Lab
+ *
+ * This Source Code Form is subject to the terms of the MIT License (MIT).
+ * A copy of the license can be found in the LICENSE file distributed with this
+ * source code.
+ */
+
+/**
+ * \file joint_distribution.hpp
+ * \date Febuary 2015
+ * \author Jan Issac (jan.issac@gmail.com)
+ */
+
+#ifndef FL__DISTRIBUTION__JOINT_DISTRIBUTION_HPP
+#define FL__DISTRIBUTION__JOINT_DISTRIBUTION_HPP
+
+#include <fl/distribution/joint_distribution_id.hpp>
+#include <fl/distribution/joint_distribution_iid.hpp>
+
+#endif
