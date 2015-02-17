e330462751aec121cc399840991f54bea909b87d
issac <jan.issac@gmail.com>
2015-02-17 20:15:09 +0100

    Added JointObservationModel header for ID and IID

---
+++ include/fl/model/observation/joint_observation_model.hpp
@@ -0,0 +1,28 @@
+/*
+ * This is part of the FL library, a C++ Bayesian filtering library
+ * (https://github.com/filtering-library)
+ *
+ * Copyright (c) 2014 Jan Issac (jan.issac@gmail.com)
+ * Copyright (c) 2014 Manuel Wuthrich (manuel.wuthrich@gmail.com)
+ *
+ * Max-Planck Institute for Intelligent Systems, AMD Lab
+ * University of Southern California, CLMC Lab
+ *
+ * This Source Code Form is subject to the terms of the MIT License (MIT).
+ * A copy of the license can be found in the LICENSE file distributed with this
+ * source code.
+ */
+
+/**
+ * \file joint_observation_model.hpp
+ * \date Febuary 2015
+ * \author Jan Issac (jan.issac@gmail.com)
+ */
+
+#ifndef FL__MODEL__OBSERVATION__JOINT_OBSERVATION_MODEL_HPP
+#define FL__MODEL__OBSERVATION__JOINT_OBSERVATION_MODEL_HPP
+
+#include <fl/model/observation/joint_observation_model_id.hpp>
+#include <fl/model/observation/joint_observation_model_iid.hpp>
+
+#endif
