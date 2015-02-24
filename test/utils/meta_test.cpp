a3baaa4eebf8236f0649af192b6cb0ea90577e7c
issac <jan.issac@gmail.com>
2015-02-24 18:55:27 +0100

    Added CombineOptions tests

--- test/utils/meta_test.cpp
+++ test/utils/meta_test.cpp
@@ -29,46 +29,112 @@
 
 #include <cmath>
 
+using namespace fl;
+
 TEST(MetaTests, ExpandSizes)
 {
-    EXPECT_EQ((fl::ExpandSizes<-1, 10>::Size), -1);
-    EXPECT_EQ((fl::ExpandSizes<10, -1>::Size), -1);
-    EXPECT_EQ((fl::ExpandSizes<-1, -1>::Size), -1);
+    EXPECT_EQ((ExpandSizes<-1, 10>::Size), -1);
+    EXPECT_EQ((ExpandSizes<10, -1>::Size), -1);
+    EXPECT_EQ((ExpandSizes<-1, -1>::Size), -1);
 
-    EXPECT_EQ((fl::ExpandSizes<1, 1>::Size), 1);
-    EXPECT_EQ((fl::ExpandSizes<1, 10>::Size), 10);
+    EXPECT_EQ((ExpandSizes<1, 1>::Size), 1);
+    EXPECT_EQ((ExpandSizes<1, 10>::Size), 10);
 
-    EXPECT_EQ((fl::ExpandSizes<2, 10>::Size), 20);
-    EXPECT_EQ((fl::ExpandSizes<3, 5>::Size), 15);
+    EXPECT_EQ((ExpandSizes<2, 10>::Size), 20);
+    EXPECT_EQ((ExpandSizes<3, 5>::Size), 15);
 }
 
 
 TEST(MetaTests, JoinSizes_single_param)
 {
-    EXPECT_EQ((fl::JoinSizes<0>::Size), 0);
-    EXPECT_EQ((fl::JoinSizes<-1>::Size), -1);
-    EXPECT_EQ((fl::JoinSizes<1>::Size), 1);
-    EXPECT_EQ((fl::JoinSizes<2>::Size), 2);
+    EXPECT_EQ((JoinSizes<0>::Size), 0);
+    EXPECT_EQ((JoinSizes<-1>::Size), -1);
+    EXPECT_EQ((JoinSizes<1>::Size), 1);
+    EXPECT_EQ((JoinSizes<2>::Size), 2);
 }
 
 TEST(MetaTests, JoinSizes_two_param)
 {
-    EXPECT_EQ((fl::JoinSizes<-1, -1>::Size), -1);
-    EXPECT_EQ((fl::JoinSizes<-1,  1>::Size), -1);
-    EXPECT_EQ((fl::JoinSizes< 1, -1>::Size), -1);
-    EXPECT_EQ((fl::JoinSizes< 1,  1>::Size),  2);
+    EXPECT_EQ((JoinSizes<-1, -1>::Size), -1);
+    EXPECT_EQ((JoinSizes<-1,  1>::Size), -1);
+    EXPECT_EQ((JoinSizes< 1, -1>::Size), -1);
+    EXPECT_EQ((JoinSizes< 1,  1>::Size),  2);
 
-    // EXPECT_EQ((fl::JoinSizes<-1,  0>::Size), -1); // should be rejected
-    // EXPECT_EQ((fl::JoinSizes< 1,  0>::Size),  2); // should be rejected
+    // EXPECT_EQ((JoinSizes<-1,  0>::Size), -1); // should be rejected
+    // EXPECT_EQ((JoinSizes< 1,  0>::Size),  2); // should be rejected
 }
 
 TEST(MetaTests, JoinSizes_multiple_param)
 {
-    EXPECT_EQ((fl::JoinSizes<-1, -1, 1, 2, -1>::Size), -1);
-    EXPECT_EQ((fl::JoinSizes< 1, -1, 1, 2, -1>::Size), -1);
-    EXPECT_EQ((fl::JoinSizes< 1,  1, 1, 2, -1>::Size), -1);
-    EXPECT_EQ((fl::JoinSizes<-1,  1, 1, 2, -1>::Size), -1);
-    EXPECT_EQ((fl::JoinSizes< 1, -1, 1, 2, -1>::Size), -1);
-    EXPECT_EQ((fl::JoinSizes<1, 1, 1, 1, 1>::Size), 5);
-    EXPECT_EQ((fl::JoinSizes<1, 2, 3, 4, 5>::Size), 5*6/2);
+    EXPECT_EQ((JoinSizes<-1, -1, 1, 2, -1>::Size), -1);
+    EXPECT_EQ((JoinSizes< 1, -1, 1, 2, -1>::Size), -1);
+    EXPECT_EQ((JoinSizes< 1,  1, 1, 2, -1>::Size), -1);
+    EXPECT_EQ((JoinSizes<-1,  1, 1, 2, -1>::Size), -1);
+    EXPECT_EQ((JoinSizes< 1, -1, 1, 2, -1>::Size), -1);
+    EXPECT_EQ((JoinSizes<1, 1, 1, 1, 1>::Size), 5);
+    EXPECT_EQ((JoinSizes<1, 2, 3, 4, 5>::Size), 5*6/2);
 }
+
+
+TEST(MetaTests, CombineOptions_NoOptions)
+{
+    EXPECT_EQ(CombineOptions<>::Options::Value, NoOptions);
+    EXPECT_EQ(CombineOptions<NoOptions>::Options::Value, NoOptions);
+
+    EXPECT_EQ(
+        CombineOptions<>::Options::Value, Options<NoOptions>::Value);
+    EXPECT_EQ(
+        CombineOptions<NoOptions>::Options::Value, Options<NoOptions>::Value);
+}
+
+
+TEST(MetaTests, CombineOptions_single_argument)
+{
+    EXPECT_EQ(
+        CombineOptions<AdditiveProcessNoise>::Options::Value,
+        AdditiveProcessNoise
+    );
+
+    EXPECT_EQ(
+        CombineOptions<AdditiveProcessNoise>::Options::Value,
+        Options<AdditiveProcessNoise>::Value
+    );
+}
+
+TEST(MetaTests, CombineOptions_two_argument)
+{
+    EXPECT_EQ(
+        (CombineOptions<AdditiveProcessNoise,
+         AdditiveObsrvNoise>::Options::Value),
+        AdditiveProcessNoise | AdditiveObsrvNoise
+    );
+
+    EXPECT_EQ(
+        (CombineOptions<AdditiveProcessNoise,
+         AdditiveObsrvNoise>::Options::Value),
+        Options<AdditiveProcessNoise | AdditiveObsrvNoise>::Value
+    );
+}
+
+TEST(MetaTests, CombineOptions_three_argument)
+{
+    EXPECT_EQ(
+        (CombineOptions<
+            AdditiveProcessNoise,
+            AdditiveObsrvNoise,
+            FactorizeParams>::Options::Value),
+        AdditiveProcessNoise | AdditiveObsrvNoise | FactorizeParams
+    );
+
+    EXPECT_EQ(
+        (CombineOptions<
+             AdditiveProcessNoise,
+             AdditiveObsrvNoise,
+             FactorizeParams>::Options::Value),
+        Options<
+            AdditiveProcessNoise    |
+            AdditiveObsrvNoise      |
+            FactorizeParams>::Value
+    );
+}
+
