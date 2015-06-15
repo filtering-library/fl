12124f8ac5f30fbdab8fa17730a357e43355d951
manuel <manuel.wuthrich@gmail.com>
2015-06-15 10:02:38 +0200

    particle filter implemented and tested

--- test/particle_filter/particle_filter_test.cpp
+++ test/particle_filter/particle_filter_test.cpp
@@ -201,7 +201,6 @@ TEST(particle_filter, update)
     srand(0);
     size_t N_particles = 10000;
     size_t N_steps = 10;
-    size_t delta_time = 1;
 
 
     // create process model
@@ -248,13 +247,7 @@ TEST(particle_filter, update)
         Observation observation(0.5, 0.5, 0.5);
 
         particle_filter.update(observation, particle_belief, particle_belief);
-
         gaussian_filter.update(observation, gaussian_belief, gaussian_belief);
-
-        std::cout << "gaussian mean " << gaussian_belief.mean().transpose() << std::endl;
-        std::cout << "gaussian cov " << std::endl << gaussian_belief.covariance() << std::endl;
-        std::cout << "particle mean " << particle_belief.mean().transpose() << std::endl;
-        std::cout << "particle cov " << std::endl << particle_belief.covariance() << std::endl;
 
         EXPECT_TRUE(moments_are_similar(
                         particle_belief.mean(), particle_belief.covariance(),
@@ -262,6 +255,121 @@ TEST(particle_filter, update)
     }
 
 }
+
+
+
+
+
+
+
+
+
+TEST(particle_filter, predict_and_update)
+{
+    typedef Eigen::Matrix<double, 3, 1> State;
+    typedef Eigen::Matrix<double, 3, 1> Observation;
+    typedef Eigen::Matrix<double, 3, 1> Input;
+
+    typedef Eigen::Matrix<double, 3, 3> Matrix;
+
+    typedef fl::LinearGaussianProcessModel<State, Input> ProcessModel;
+    typedef fl::LinearObservationModel<Observation, State> ObservationModel;
+
+    typedef fl::LinearGaussianObservationModel<Observation, State>
+                                                        OldObservationModel;
+
+
+    // particle filter
+    typedef fl::ParticleFilter<ProcessModel, ObservationModel> ParticleFilter;
+    typedef ParticleFilter::StateDistribution ParticleBelief;
+
+    // gaussian filter
+    typedef fl::GaussianFilter<ProcessModel, OldObservationModel> GaussianFilter;
+    typedef GaussianFilter::StateDistribution GaussianBelief;
+
+
+    srand(0);
+    size_t N_particles = 1000;
+    size_t N_steps = 100;
+    size_t delta_time = 1;
+
+
+    // create process model
+    ProcessModel process_model;
+    {
+        process_model.A(some_rotation());
+        Matrix R = some_rotation();
+        Matrix D = Eigen::DiagonalMatrix<double, 3>(1, 3.5, 1.2);
+        process_model.covariance(R*D*R.transpose());
+    }
+
+    // create observation model
+    /// \todo this is a hack because the GF does not currently work with the new
+    /// observation model interface
+    OldObservationModel old_observation_model;
+    {
+        old_observation_model.H(some_rotation());
+        Matrix R = some_rotation();
+        Matrix D = Eigen::DiagonalMatrix<double, 3>(3.1, 1.0, 1.3);
+        old_observation_model.covariance(R*D*R.transpose());
+    }
+
+    ObservationModel observation_model;
+    observation_model.sensor_matrix(old_observation_model.H());
+    observation_model.noise_matrix(old_observation_model.square_root());
+
+    // create filters
+    ParticleFilter particle_filter(process_model, observation_model);
+    GaussianFilter gaussian_filter(process_model, old_observation_model);
+
+    // create intial beliefs
+    GaussianBelief gaussian_belief;
+    {
+        gaussian_belief.mean(State::Zero());
+        gaussian_belief.covariance(Matrix::Identity());
+    }
+    ParticleBelief particle_belief;
+    particle_belief.from_distribution(gaussian_belief, N_particles);
+
+
+    fl::StandardGaussian<State> standard_gaussian;
+    State state = gaussian_belief.sample();
+    // run prediction
+    for(size_t i = 0; i < N_steps; i++)
+    {
+        // simulate system
+        state = process_model.predict_state(delta_time,
+                                            state,
+                                            standard_gaussian.sample(),
+                                            State::Zero());
+        Observation observation =
+                observation_model.observation(state, standard_gaussian.sample());
+
+        // predict
+        particle_filter.predict(delta_time, State::Zero(),
+                                particle_belief, particle_belief);
+        gaussian_filter.predict(delta_time, State::Zero(),
+                                gaussian_belief, gaussian_belief);
+
+        // update
+        particle_filter.update(observation, particle_belief, particle_belief);
+        gaussian_filter.update(observation, gaussian_belief, gaussian_belief);
+    }
+
+    State delta = particle_belief.mean() - gaussian_belief.mean();
+    double mh_distance = delta.transpose() * gaussian_belief.precision() * delta;
+
+    // make sure that the estimate of the pf is within one std dev
+    EXPECT_TRUE(std::sqrt(mh_distance) <= 1.0);
+}
+
+
+
+
+
+
+
+
 
 
 
