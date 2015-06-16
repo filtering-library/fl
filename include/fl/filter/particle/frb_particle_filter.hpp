5c0f047068af24a9ab0bfd0473cb6dda3d8851b4
manuel <manuel.wuthrich@gmail.com>
2015-06-16 22:53:39 +0200

    started implementing frb_particle_filter

---
+++ include/fl/filter/particle/frb_particle_filter.hpp
@@ -0,0 +1,269 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2014 Max-Planck-Institute for Intelligent Systems,
+ *                     University of Southern California
+ *    Jan Issac (jan.issac@gmail.com)
+ *    Manuel Wuthrich (manuel.wuthrich@gmail.com)
+ *
+ *
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * @date 2015
+ * @author Manuel Wuthrich (manuel.wuthrich@gmail.com)
+ * Max-Planck-Institute for Intelligent Systems
+ */
+
+
+#ifndef FL__FILTER__PARTICLE__FRB_PARTICLE_FILTER_HPP
+#define FL__FILTER__PARTICLE__FRB_PARTICLE_FILTER_HPP
+
+
+#include <fl/distribution/discrete_distribution.hpp>
+#include <fl/distribution/standard_gaussian.hpp>
+
+
+/// \todo: REMOVE UNNECESSARY INCLUDES
+
+#include <map>
+#include <tuple>
+#include <memory>
+
+#include <fl/util/meta.hpp>
+#include <fl/util/traits.hpp>
+#include <fl/util/profiling.hpp>
+
+#include <fl/exception/exception.hpp>
+#include <fl/filter/filter_interface.hpp>
+
+
+#include <fl/model/observation/joint_observation_model.hpp>
+
+#include <Eigen/Core>
+
+namespace fl
+{
+
+template <typename...> class FRBParticleFilter;
+
+/**
+ * FRBParticleFilter Traits
+ */
+template <typename StateTransitionFunction,
+          typename SwitchingDensity,
+          typename ObservationDensity>
+struct Traits<FRBParticleFilter<StateTransitionFunction,
+                                SwitchingDensity,
+                                ObservationDensity>>
+{
+
+    /// \todo: does this make sense to just assume the state ans obsrv to be vectorxd?
+    typedef typename Eigen::VectorXd State;
+    typedef typename Eigen::VectorXd Obsrv;
+    //    typedef typename StateTransitionFunction::State        State;
+    //    typedef typename ObservationDensity::Obsrv             Obsrv;
+
+    typedef typename StateTransitionFunction::Input        Input;
+
+    typedef DiscreteDistribution<State> StateDistribution;
+
+    /** \cond INTERNAL */
+    typedef typename StateTransitionFunction::Noise      ProcessNoise;
+};
+
+template <typename StateTransitionFunction,
+          typename SwitchingDensity,
+          typename ObservationDensity>
+struct FRBParticleFilter<StateTransitionFunction,
+                         SwitchingDensity,
+                         ObservationDensity>
+    :
+    public FilterInterface<FRBParticleFilter<StateTransitionFunction,
+                                             SwitchingDensity,
+                                             ObservationDensity>>
+{
+private:
+    typedef FRBParticleFilter<StateTransitionFunction,
+                              SwitchingDensity,
+                              ObservationDensity> This;
+
+    typedef from_traits(ProcessNoise);
+
+public:
+    typedef from_traits(State);
+    typedef from_traits(Input);
+    typedef from_traits(Obsrv);
+    typedef from_traits(StateDistribution);
+
+    typedef typename Eigen::Array<SwitchingDensity,
+                                  Eigen::Dynamic, 1> SwitchingDensities;
+    typedef typename Eigen::Array<ObservationDensity,
+                                  Eigen::Dynamic, 2> ObservationDensities;
+
+public:
+    FRBParticleFilter(const StateTransitionFunction& trans_function,
+                      const SwitchingDensities& switch_densities,
+                      const ObservationDensities& obsrv_densities,
+                      const double& max_kl_divergence = 1.0)
+        : trans_function_(trans_function),
+          switch_densities_(switch_densities),
+          obsrv_densities_(obsrv_densities),
+          process_noise_(trans_function.noise_dimension()),
+          max_kl_divergence_(max_kl_divergence)
+    { }
+
+
+    /// predict ****************************************************************
+    virtual void predict(double dt,
+                         const Input& input,
+                         const StateDistribution& prior_dist,
+                         StateDistribution& predicted_dist)
+    {
+        predicted_dist = prior_dist;
+        int global_dim = trans_function_.state_dimension();
+        int local_dim = switch_densities_.size();
+
+
+        // the global state part is propagated as in the standard particle filter
+        for(size_t i = 0; i < predicted_dist.size(); i++)
+        {
+            Eigen::VectorXd& next_state =
+                    predicted_dist.location(i).topRows(global_dim);
+
+            Eigen::VectorXd& state = prior_dist.location(i).topRows(global_dim);
+
+            next_state = trans_function_.predict_state(dt,
+                                                       state,
+                                                       process_noise_.sample(),
+                                                       input);
+        }
+
+        // the local state part is propagated analytically
+        for(size_t i = 0; i < predicted_dist.size(); i++)
+        {
+            Eigen::VectorXd& next_state =
+                    predicted_dist.location(i).bottomRows(local_dim);
+
+            Eigen::VectorXd& state =
+                    prior_dist.location(i).bottomRows(local_dim);
+
+            for(int j = 0; j < state.size(); j++)
+            {
+                next_state[j] =
+                     switch_densities_[j].probability(1, 1, dt) * state[j]
+                   + switch_densities_[j].probability(1, 0, dt) * (1.-state[j]);
+            }
+        }
+    }
+
+    /// update *****************************************************************
+    virtual void update(const Obsrv& obsrv,
+                        const StateDistribution& predicted_dist,
+                        StateDistribution& posterior_dist)
+    {
+        // if the samples are too concentrated then resample
+        if(predicted_dist.kl_given_uniform() > max_kl_divergence_)
+        {
+            posterior_dist.from_distribution(predicted_dist,
+                                             predicted_dist.size());
+        }
+        else
+        {
+            posterior_dist = predicted_dist;
+        }
+
+        // update the weights of the particles with the likelihoods
+        posterior_dist.delta_log_prob_mass(
+             obsrv_density_.log_probabilities(obsrv, predicted_dist.locations()));
+    }
+
+    /// predict and update *****************************************************
+    virtual void predict_and_update(double dt,
+                                    const Input& input,
+                                    const Obsrv& observation,
+                                    const StateDistribution& prior_dist,
+                                    StateDistribution& posterior_dist)
+    {
+        predict(dt, input, prior_dist, posterior_dist);
+        update(observation, posterior_dist, posterior_dist);
+    }
+
+
+    /// set and get ************************************************************
+    StateTransitionFunction& trans_function()
+    {
+        return trans_function_;
+    }
+    ObservationDensity& obsrv_density()
+    {
+        return obsrv_density_;
+    }
+
+    const StateTransitionFunction& trans_function() const
+    {
+        return trans_function_;
+    }
+
+    const ObservationDensity& obsrv_density() const
+    {
+        return obsrv_density_;
+    }
+
+    /// \todo: should this function be here?
+    virtual StateDistribution create_state_distribution() const
+    {
+        auto state_distr = StateDistribution(trans_function().state_dimension());
+
+        return state_distr;
+    }
+
+protected:
+    StateTransitionFunction trans_function_;
+    ObservationDensity obsrv_density_;
+
+    SwitchingDensities switch_densities_;
+    ObservationDensities obsrv_densities_;
+
+
+    StandardGaussian<ProcessNoise> process_noise_;
+
+    // when the KL divergence KL(p||u), where p is the particle distribution
+    // and u is the uniform distribution, exceeds max_kl_divergence_, then there
+    // is a resampling step. can be understood as -log(f) where f is the
+    // fraction of nonzero particles.
+    double max_kl_divergence_;
+};
+
+}
+
+
+#endif
---
+++ test/particle_filter/frb_particle_filter_test.cpp
@@ -0,0 +1,149 @@
+/*
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2014 Max-Planck-Institute for Intelligent Systems,
+ *                     University of Southern California
+ *    Jan Issac (jan.issac@gmail.com)
+ *    Manuel Wuthrich (manuel.wuthrich@gmail.com)
+ *
+ *
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * @date 2015
+ * @author Manuel Wuthrich (manuel.wuthrich@gmail.com)
+ * Max-Planck-Institute for Intelligent Systems
+ */
+
+
+#include <gtest/gtest.h>
+
+#include <Eigen/Core>
+
+#include <fl/util/math/linear_algebra.hpp>
+
+#include <fl/filter/particle/frb_particle_filter.hpp>
+#include <fl/filter/gaussian/gaussian_filter_kf.hpp>
+#include <fl/model/observation/linear_observation_model.hpp>
+#include <fl/model/process/linear_process_model.hpp>
+#include <fl/model/process/binary_transition_density.hpp>
+
+template<typename Vector, typename Matrix>
+bool moments_are_similar(Vector mean_a, Matrix cov_a,
+                         Vector mean_b, Matrix cov_b, double epsilon = 0.1)
+{
+    Matrix cov_delta = cov_a.inverse() * cov_b;
+    bool are_similar = cov_delta.isApprox(Matrix::Identity(), epsilon);
+
+    Matrix square_root = fl::matrix_sqrt(cov_a);
+    double max_mean_delta =
+            (square_root.inverse() * (mean_a-mean_b)).cwiseAbs().maxCoeff();
+
+    are_similar = are_similar && max_mean_delta < epsilon;
+
+    return are_similar;
+}
+
+
+Eigen::Matrix<double, 3, 3> some_rotation()
+{
+    double angle = 2 * M_PI * double(rand()) / double(RAND_MAX);
+
+    Eigen::Matrix<double, 3, 3> R = Eigen::Matrix<double, 3, 3>::Identity();
+
+    R = R * Eigen::AngleAxisd(angle, Eigen::Vector3d::UnitX());
+    R = R * Eigen::AngleAxisd(angle, Eigen::Vector3d::UnitZ());
+    R = R * Eigen::AngleAxisd(angle, Eigen::Vector3d::UnitY());
+    return R;
+}
+
+
+TEST(particle_filter, predict)
+{
+    typedef Eigen::Matrix<double, 3, 1> State;
+    typedef Eigen::Matrix<double, 3, 1> Observation;
+    typedef Eigen::Matrix<double, 3, 1> Input;
+
+    typedef Eigen::Matrix<double, 3, 3> Matrix;
+
+    typedef fl::LinearGaussianProcessModel<State, Input> ProcessModel;
+    typedef fl::BinaryTransitionDensity SwitchingDensity;
+    typedef fl::LinearObservationModel<Observation, State> ObservationModel;
+
+    // particle filter
+    typedef fl::FRBParticleFilter<ProcessModel,
+                                  SwitchingDensity,
+                                  ObservationModel> FRBParticleFilter;
+    typedef FRBParticleFilter::StateDistribution FRBParticleBelief;
+
+    // gaussian filter
+    typedef fl::GaussianFilter<ProcessModel, ObservationModel> GaussianFilter;
+    typedef GaussianFilter::StateDistribution GaussianBelief;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
