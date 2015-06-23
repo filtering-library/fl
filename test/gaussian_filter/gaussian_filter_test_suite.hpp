6ce6672ade6f9c9c447a2e7d159d4f87ac0574bd
issac <jan.issac@gmail.com>
2015-06-23 19:20:21 +0200

    Created gaussian_filter_test_suite

---
+++ test/gaussian_filter/gaussian_filter_test_suite.hpp
@@ -0,0 +1,36 @@
+/*
+ * This is part of the FL library, a C++ Bayesian filtering library
+ * (https://github.com/filtering-library)
+ *
+ * Copyright (c) 2014 Jan Issac (jan.issac@gmail.com)
+ * Copyright (c) 2014 Manuel Wuthrich (manuel.wuthrich@gmail.com)
+ *
+ * Max-Planck Institute for Intelligent Systems, AMD Lab
+ * University of Southern California, CLMC Lab
+ *
+ * This Source Code Form is subject to the terms of the MIT License (MIT).
+ * A copy of the license can be found in the LICENSE file distributed with this
+ * source code.
+ */
+
+/**
+ * \file gaussian_filter_test_suite.hpp
+ * \date June 2015
+ * \author Jan Issac (jan.issac@gmail.com)
+ */
+
+#include <gtest/gtest.h>
+#include "../typecast.hpp"
+
+namespace fl
+{
+
+template <typename TestType>
+class GaussianFilterTest
+    : public ::testing::Test
+{
+public:
+
+};
+
+}
