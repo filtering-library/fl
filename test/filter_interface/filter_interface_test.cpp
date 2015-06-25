3cd97e02ace4a7eca7b8b2647771c2e84c83d9ac
issac <jan.issac@gmail.com>
2015-06-25 12:36:29 +0200

    Tests and license update

--- test/filter_interface/filter_interface_test.cpp
+++ test/filter_interface/filter_interface_test.cpp
@@ -1,47 +1,22 @@
 /*
- * Software License Agreement (BSD License)
+ * This is part of the FL library, a C++ Bayesian filtering library
+ * (https://github.com/filtering-library)
  *
- *  Copyright (c) 2014 Max-Planck-Institute for Intelligent Systems,
- *                     University of Southern California
- *    Jan Issac (jan.issac@gmail.com)
- *    Manuel Wuthrich (manuel.wuthrich@gmail.com)
+ * Copyright (c) 2014 Jan Issac (jan.issac@gmail.com)
+ * Copyright (c) 2014 Manuel Wuthrich (manuel.wuthrich@gmail.com)
  *
+ * Max-Planck Institute for Intelligent Systems, AMD Lab
+ * University of Southern California, CLMC Lab
  *
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *   * Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *   * Redistributions in binary form must reproduce the above
- *     copyright notice, this list of conditions and the following
- *     disclaimer in the documentation and/or other materials provided
- *     with the distribution.
- *   * Neither the name of Willow Garage, Inc. nor the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
- *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
- *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
- *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
- *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
- *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- *
+ * This Source Code Form is subject to the terms of the MIT License (MIT).
+ * A copy of the license can be found in the LICENSE file distributed with this
+ * source code.
  */
 
 /**
- * \date 2014
+ * \file filter_interface_test.cpp
+ * \date October 2014
  * \author Jan Issac (jan.issac@gmail.com)
- * Max-Planck-Institute for Intelligent Systems,
- * University of Southern California
  */
 
 #include <gtest/gtest.h>
@@ -59,7 +34,7 @@ template <class FilterAlgorithm>
 class FilterContext
 {
 public:
-    typedef std::shared_ptr<FilterAlgorithm> Filter;
+    typedef FilterAlgorithm Filter;
 
     FilterContext(Filter _filter)
         : filter_(_filter),
@@ -71,12 +46,12 @@ public:
 
     void predict()
     {
-        filter_->predict(1.0, u_, dist_, dist_);
+        filter_.predict(dist_, u_, dist_);
     }
 
     void update()
     {
-        filter_->update(dist_, y_, dist_);
+        filter_.update(dist_, y_, dist_);
     }
 
     Filter& filter()
@@ -92,10 +67,7 @@ public:
 
 TEST(FilterInterface, NonTemplatedFilter)
 {
-    typedef FilterForFun FilterAlgo;
-
-    FilterContext<FilterAlgo> filter_context =
-        FilterContext<FilterAlgo>(std::make_shared<FilterAlgo>());
+    auto filter_context = FilterContext<FilterForFun>(FilterForFun());
 
     // predict pre-condition
     EXPECT_DOUBLE_EQ(filter_context.dist_, 1.);
@@ -114,25 +86,25 @@ TEST(FilterInterface, NonTemplatedFilter)
     EXPECT_DOUBLE_EQ(filter_context.y_, 2.);
 }
 
-//TEST(FilterInterface, TemplatedFilter)
-//{
-//    typedef FilterForMoreFun<int, long int, long long int> FilterAlgo;
+TEST(FilterInterface, TemplatedFilter)
+{
+    typedef FilterForMoreFun<int, long int, long long int> FilterAlgo;
 
-//    FilterContext<FilterAlgo> filter_context(std::make_shared<FilterAlgo>());
+    auto filter_context = FilterContext<FilterAlgo>(FilterAlgo());
 
-//    // predict pre-condition
-//    EXPECT_DOUBLE_EQ(filter_context.dist_, 1.);
-//    EXPECT_DOUBLE_EQ(filter_context.y_, 2.);
+    // predict pre-condition
+    EXPECT_DOUBLE_EQ(filter_context.dist_, 1.);
+    EXPECT_DOUBLE_EQ(filter_context.y_, 2.);
 
-//    EXPECT_NO_THROW(filter_context.predict());
+    EXPECT_NO_THROW(filter_context.predict());
 
-//    // predict post-condition
-//    EXPECT_DOUBLE_EQ(filter_context.dist_, 3.);
-//    EXPECT_DOUBLE_EQ(filter_context.y_, 2.);
+    // predict post-condition
+    EXPECT_DOUBLE_EQ(filter_context.dist_, 3.);
+    EXPECT_DOUBLE_EQ(filter_context.y_, 2.);
 
-//    EXPECT_NO_THROW(filter_context.update());
+    EXPECT_NO_THROW(filter_context.update());
 
-//    // update post-condition
-//    EXPECT_DOUBLE_EQ(filter_context.dist_, (3. + 2.)/3. );
-//    EXPECT_DOUBLE_EQ(filter_context.y_, 2.);
-//}
+    // update post-condition
+    EXPECT_DOUBLE_EQ(filter_context.dist_, (3. + 2.)/3. );
+    EXPECT_DOUBLE_EQ(filter_context.y_, 2.);
+}
