c1bcc93bc01a3dffb502d2967a9471850eaff480
issac <jan.issac@gmail.com>
2015-06-25 16:59:58 +0200

    Transformed test into a typed and parametrized test set

--- test/model/state_transition/linear_state_transition_model_test.cpp
+++ test/model/state_transition/linear_state_transition_model_test.cpp
@@ -20,6 +20,7 @@
  */
 
 #include <gtest/gtest.h>
+#include "../../typecast.hpp"
 
 #include <Eigen/Dense>
 
@@ -29,29 +30,31 @@
 #include <fl/util/types.hpp>
 #include <fl/model/process/linear_process_model.hpp>
 
-class LinearStateTransitionModelTests:
+template <typename TestType>
+class LinearStateTransitionModelTest:
     public testing::Test
 {
 public:
     enum: signed int
     {
-        StateDim = 10,
-        InputDim = 10
+        StateDim = TestType::Parameter::StateDim,
+        InputDim = TestType::Parameter::InputDim,
+
+        StateSize = fl::TestSize<StateDim, TestType>::Value,
+        InputSize = fl::TestSize<InputDim, TestType>::Value
     };
 
     typedef Eigen::Matrix<fl::Real, StateDim, 1> State;
     typedef Eigen::Matrix<fl::Real, InputDim, 1> Input;
     typedef fl::LinearStateTransitionModel<State, Input> LinearModel;
 
-    typedef Eigen::Matrix<fl::Real, Eigen::Dynamic, 1> StateX;
-    typedef Eigen::Matrix<fl::Real, Eigen::Dynamic, 1> InputX;
-    typedef fl::LinearStateTransitionModel<StateX, InputX> LinearModelX;
-
     typedef typename LinearModel::Noise Noise;
-    typedef typename LinearModelX::Noise NoiseX;
 
-    template <typename Model>
-    void init_dimension_test(Model&& model)
+    LinearStateTransitionModelTest()
+        : model()
+    { }
+
+    void init_dimension_test()
     {
         EXPECT_EQ(model.state_dimension(), StateDim);
         EXPECT_EQ(model.noise_dimension(), StateDim);
@@ -64,20 +67,17 @@ public:
         EXPECT_EQ(model.noise_matrix().cols(), StateDim);
     }
 
-    template <typename Model>
-    void init_dynamics_matrix_value_test(Model&& model)
+    void init_dynamics_matrix_value_test()
     {
         EXPECT_TRUE(model.dynamics_matrix().isIdentity());
     }
 
-    template <typename Model>
-    void init_noise_matrix_value_test(Model&& model)
+    void init_noise_matrix_value_test()
     {
         EXPECT_TRUE(model.noise_matrix().isIdentity());
     }
 
-    template <typename Model>
-    void dynamics_matrix_value_test(Model&& model)
+    void dynamics_matrix_value_test()
     {
         auto dynamics_matrix = model.dynamics_matrix();
         dynamics_matrix.setRandom();
@@ -85,8 +85,7 @@ public:
         EXPECT_TRUE(fl::are_similar(model.dynamics_matrix(), dynamics_matrix));
     }
 
-    template <typename Model>
-    void noise_matrix_value_test(Model&& model)
+    void noise_matrix_value_test()
     {
         auto noise_matrix = model.noise_matrix();
         noise_matrix.setRandom();
@@ -94,8 +93,7 @@ public:
         EXPECT_TRUE(fl::are_similar(model.noise_matrix(), noise_matrix));
     }
 
-    template <typename Model>
-    void expected_state_test(Model&& model)
+    void expected_state_test()
     {
         auto x = State(model.state_dimension());
         auto u = Input(model.input_dimension());
@@ -103,11 +101,13 @@ public:
         x.setRandom();
         u.setRandom();
 
-        EXPECT_TRUE(fl::are_similar(model.expected_state(x, u), x + u));
+        EXPECT_TRUE(
+            fl::are_similar(
+                model.expected_state(x, u),
+                x + model.input_matrix() * u));
     }
 
-    template <typename Model>
-    void state_with_zero_noise_test(Model&& model)
+    void state_with_zero_noise_test()
     {
         auto x = State(model.state_dimension());
         auto u = Input(model.input_dimension());
@@ -117,11 +117,13 @@ public:
         u.setRandom();
         w.setZero();
 
-        EXPECT_TRUE(fl::are_similar(model.state(x, u, w), x + u));
+        EXPECT_TRUE(
+            fl::are_similar(
+                model.state(x, w, u),
+                x + model.input_matrix() * u));
     }
 
-    template <typename Model>
-    void state_test(Model&& model)
+    void state_test()
     {
         auto x = State(model.state_dimension());
         auto u = Input(model.input_dimension());
@@ -131,144 +133,85 @@ public:
         u.setRandom();
         w.setRandom();
 
-        EXPECT_TRUE(fl::are_similar(model.state(x, u, w), x + u + w));
+        EXPECT_TRUE(
+            fl::are_similar(
+                model.state(x, w, u),
+                x + model.input_matrix() * u + w));
     }
+
+protected:
+    LinearModel model;
 };
 
-// == Initial dimension tests =============================================== //
-
-TEST_F(LinearStateTransitionModelTests, init_fixed_dimension)
+template <int StateDimension, int InputDimension>
+struct Dimensions
 {
-    init_dimension_test(LinearModel());
+    enum: signed int
+    {
+        StateDim = StateDimension,
+        InputDim = InputDimension
+    };
+};
+
+typedef ::testing::Types<
+            fl::StaticTest<Dimensions<2, 1>>,
+            fl::StaticTest<Dimensions<2, 2>>,
+            fl::StaticTest<Dimensions<3, 3>>,
+            fl::StaticTest<Dimensions<10, 10>>,
+            fl::StaticTest<Dimensions<10, 20>>,
+            fl::StaticTest<Dimensions<100, 10>>,
+            fl::StaticTest<Dimensions<3, 100>>,
+            fl::StaticTest<Dimensions<100, 100>>,
+            fl::DynamicTest<Dimensions<2, 1>>,
+            fl::DynamicTest<Dimensions<2, 2>>,
+            fl::DynamicTest<Dimensions<3, 3>>,
+            fl::DynamicTest<Dimensions<10, 10>>,
+            fl::DynamicTest<Dimensions<10, 20>>,
+            fl::DynamicTest<Dimensions<100, 10>>,
+            fl::DynamicTest<Dimensions<3, 100>>,
+            fl::DynamicTest<Dimensions<100, 100>>
+        > TestTypes;
+
+TYPED_TEST_CASE(LinearStateTransitionModelTest, TestTypes);
+
+TYPED_TEST(LinearStateTransitionModelTest, init_dimension)
+{
+    TestFixture::init_dimension_test();
 }
 
-TEST_F(LinearStateTransitionModelTests, init_fixed_dimension_dynamic_constr)
+TYPED_TEST(LinearStateTransitionModelTest, init_dynamics_matrix_value)
 {
-    init_dimension_test(LinearModel(StateDim, InputDim));
+    TestFixture::init_dynamics_matrix_value_test();
+}
+TYPED_TEST(LinearStateTransitionModelTest, init_noise_matrix_value)
+{
+    TestFixture::init_noise_matrix_value_test();
 }
 
-TEST_F(LinearStateTransitionModelTests, init_dynamic_dimension)
+TYPED_TEST(LinearStateTransitionModelTest, dynamics_matrix)
 {
-    init_dimension_test(LinearModelX(StateDim, InputDim));
+    TestFixture::dynamics_matrix_value_test();
 }
 
-// == Initial sensor matrix tests =========================================== //
-
-TEST_F(LinearStateTransitionModelTests, init_fixed_dynamics_matrix_value)
+TYPED_TEST(LinearStateTransitionModelTest, noise_matrix)
 {
-    init_dynamics_matrix_value_test(LinearModel());
+    TestFixture::noise_matrix_value_test();
 }
 
-TEST_F(LinearStateTransitionModelTests, init_fixed_dynamics_matrix_value_dynamic_constr)
+TYPED_TEST(LinearStateTransitionModelTest, expected_state)
 {
-    init_dynamics_matrix_value_test(LinearModel(StateDim, InputDim));
+    TestFixture::expected_state_test();
 }
 
-TEST_F(LinearStateTransitionModelTests, init_dynamic_dynamics_matrix_value)
+TYPED_TEST(LinearStateTransitionModelTest, state_with_zero_noise)
 {
-    init_dynamics_matrix_value_test(LinearModelX(StateDim, InputDim));
+    TestFixture::state_with_zero_noise_test();
 }
 
-// == Initial noise matrix tests ============================================ //
-
-TEST_F(LinearStateTransitionModelTests, init_fixed_noise_matrix_value)
+TYPED_TEST(LinearStateTransitionModelTest, state)
 {
-    init_noise_matrix_value_test(LinearModel());
+    TestFixture::state_test();
 }
 
-TEST_F(LinearStateTransitionModelTests, init_fixed_noise_matrix_value_dynamic_constr)
-{
-    init_noise_matrix_value_test(LinearModel(StateDim, InputDim));
-}
-
-TEST_F(LinearStateTransitionModelTests, init_dynamic_noise_matrix_value)
-{
-    init_noise_matrix_value_test(LinearModelX(StateDim, InputDim));
-}
-
-// == Sensor matrix tests =================================================== //
-
-TEST_F(LinearStateTransitionModelTests, dynamics_matrix_fixed)
-{
-    dynamics_matrix_value_test(LinearModel());
-}
-
-TEST_F(LinearStateTransitionModelTests, dynamics_matrix_fixed_dynamic_constr)
-{
-    dynamics_matrix_value_test(LinearModel(StateDim, InputDim));
-}
-
-TEST_F(LinearStateTransitionModelTests, dynamics_matrix_dynamic)
-{
-    dynamics_matrix_value_test(LinearModelX(StateDim, InputDim));
-}
-
-// == Noise matrix tests ==================================================== //
-
-TEST_F(LinearStateTransitionModelTests, noise_matrix_fixed)
-{
-    noise_matrix_value_test(LinearModel());
-}
-
-TEST_F(LinearStateTransitionModelTests, noise_matrix_fixed_dynamic_constr)
-{
-    noise_matrix_value_test(LinearModel(StateDim, InputDim));
-}
-
-TEST_F(LinearStateTransitionModelTests, noise_matrix_dynamic)
-{
-    noise_matrix_value_test(LinearModelX(StateDim, InputDim));
-}
-
-// == expected_observation ================================================== //
-
-TEST_F(LinearStateTransitionModelTests, expected_state_fixed)
-{
-    expected_state_test(LinearModel());
-}
-
-TEST_F(LinearStateTransitionModelTests, expected_state_dynamic_constr)
-{
-    expected_state_test(LinearModel(StateDim, InputDim));
-}
-
-TEST_F(LinearStateTransitionModelTests, expected_state_dynamic)
-{
-    expected_state_test(LinearModelX(StateDim, InputDim));
-}
-
-// == observation with zero noise =========================================== //
-
-TEST_F(LinearStateTransitionModelTests, state_with_zero_noise_fixed)
-{
-    state_with_zero_noise_test(LinearModel());
-}
-
-TEST_F(LinearStateTransitionModelTests, state_with_zero_noise_dynamic_constr)
-{
-    state_with_zero_noise_test(LinearModel(StateDim, InputDim));
-}
-
-TEST_F(LinearStateTransitionModelTests, state_with_zero_noise_dynamic)
-{
-    state_with_zero_noise_test(LinearModelX(StateDim, InputDim));
-}
-
-// == observation =========================================================== //
-
-TEST_F(LinearStateTransitionModelTests, state_fixed)
-{
-    state_test(LinearModel());
-}
-
-TEST_F(LinearStateTransitionModelTests, state_dynamic_constr)
-{
-    state_test(LinearModel(StateDim, InputDim));
-}
-
-TEST_F(LinearStateTransitionModelTests, state_dynamic)
-{
-    state_test(LinearModelX(StateDim, InputDim));
-}
 
 /// \todo missing probability and log_probability tests
