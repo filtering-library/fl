ee9569cb4e4315780f093ffef812cadeb8231d42
manuel <manuel.wuthrich@gmail.com>
2015-07-09 22:29:14 +0200

    added a process for orientations. needs to be tested

---
+++ test/model/state_transition/orientation_state_transition_function_test.cpp
@@ -0,0 +1,38 @@
+/*
+ * This is part of the FL library, a C++ Bayesian filtering library
+ * (https://github.com/filtering-library)
+ *
+ * Copyright (c) 2014 Jan Issac (jan.issac@gmail.com)
+ * Copyright (c) 2014 Manuel Wuthrich (manuel.wuthrich@gmail.com)
+ *
+ * Max-Planck Institute for Intelligent Systems, AMD Lab
+ * University of Southern California, CLMC Lab
+ *
+ * This Source Code Form is subject to the terms of the MIT License (MIT).
+ * A copy of the license can be found in the LICENSE file distributed with this
+ * source code.
+ */
+
+/**
+ * \file linear_state_transition_model_test.hpp
+ * \date October 2014
+ * \author Jan Issac (jan.issac@gmail.com)
+ */
+
+#include <gtest/gtest.h>
+
+#include <Eigen/Dense>
+
+#include <fl/util/types.hpp>
+#include <fl/model/process/orientation_state_transition_function.hpp>
+
+
+
+
+TEST(orientation_state_transition_function, bla)
+{
+    fl::OrientationStateTransitionFunction tf;
+
+    /// \todo some test of this model
+
+}
