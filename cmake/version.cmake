3a49d982e2d1c4acdb838ff3969e4ffa30d4a4f1
issac <jan.issac@gmail.com>
2015-07-21 19:41:43 +0200

    Updated version generation

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -6,8 +6,12 @@ execute_process(COMMAND git rev-list --count HEAD
                 WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
                 OUTPUT_VARIABLE FL_BUILD_VERSION
                 OUTPUT_STRIP_TRAILING_WHITESPACE)
-                
+
 execute_process(COMMAND git rev-parse --short HEAD
                 WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
                 OUTPUT_VARIABLE FL_REV_VERSION
                 OUTPUT_STRIP_TRAILING_WHITESPACE)
+
+set(PROJECT_VERSION
+    "${FL_MAJOR_VERSION}.${FL_MINOR_VERSION}.${FL_BUILD_VERSION}")
+
