5c371dbf2505786f7a3d57b0ad7dfa7ee99278e8
issac <jan.issac@gmail.com>
2015-07-28 11:04:42 +0200

    Added AdditiveNoiseModel interface which can be used in linear and non-linear models. This split avoids multiple-inheritance issues

---
+++ include/fl/model/additive_noise_model.hpp
@@ -0,0 +1,56 @@
+/*
+ * This is part of the FL library, a C++ Bayesian filtering library
+ * (https://github.com/filtering-library)
+ *
+ * Copyright (c) 2014 Jan Issac (jan.issac@gmail.com)
+ * Copyright (c) 2014 Manuel Wuthrich (manuel.wuthrich@gmail.com)
+ *
+ * Max-Planck Institute for Intelligent Systems, AMD Lab
+ * University of Southern California, CLMC Lab
+ *
+ * This Source Code Form is subject to the terms of the MIT License (MIT).
+ * A copy of the license can be found in the LICENSE file distributed with this
+ * source code.
+ */
+
+/**
+ * \file additive_noise_model.hpp
+ * \date July 2015
+ * \author Jan Issac (jan.issac@gmail.com)
+ */
+
+#ifndef FL__MODEL__ADDITIVE_NOISE_MODEL_HPP
+#define FL__MODEL__ADDITIVE_NOISE_MODEL_HPP
+
+#include <fl/util/types.hpp>
+#include <fl/util/traits.hpp>
+
+#include <Eigen/Dense>
+
+namespace fl
+{
+
+template <typename NoiseDensity>
+class AdditiveNoiseModel
+    : private internal::AdditiveNoiseModelType
+{
+public:
+    typedef internal::AdditiveNoiseModelType Type;
+
+    /**
+     * Noise model matrix \f$N_t\f$
+     */
+    typedef typename NoiseDensity::SecondMoment NoiseMatrix;
+public:
+    /**
+     * \brief Overridable default destructor
+     */
+    virtual ~AdditiveNoiseModel() { }
+
+    virtual NoiseMatrix noise_matrix() const = 0;
+    virtual NoiseMatrix noise_covariance() const = 0;
+};
+
+}
+
+#endif
