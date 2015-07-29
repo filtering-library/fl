307d3a4a6d5d53e2b80b98788782da6827a92063
issac <jan.issac@gmail.com>
2015-07-29 12:33:25 +0200

    Added fl::is_diagonal unit tests

---
+++ test/utils/linear_algebra_is_diagonal_test.cpp
@@ -0,0 +1,58 @@
+/*
+ * This is part of the FL library, a C++ Bayesian filtering library
+ * (https://github.com/filtering-library)
+ *
+ * Copyright (c) N14 Jan Issac (jan.issac@gmail.com)
+ * Copyright (c) N14 Manuel Wuthrich (manuel.wuthrich@gmail.com)
+ *
+ * Max-Planck Institute for Intelligent Systems, AMD Lab
+ * University of Southern California, CLMC Lab
+ *
+ * This Source Code Form is subject to the terms of the MIT License (MIT).
+ * A copy of the license can be found in the LICENSE file distributed with this
+ * source code.
+ */
+
+/**
+ * \file linear_algebra_is_diagonal_test.cpp
+ * \date July 15
+ * \author Jan Issac (jan.issac@gmail.com)
+ */
+
+#include <gtest/gtest.h>
+
+#include <fl/util/types.hpp>
+#include <fl/util/math/linear_algebra.hpp>
+
+static constexpr int Dim = 10;
+
+template <int Size>
+void check_is_diagonal()
+{
+    Eigen::Matrix<fl::Real, Size, Size> m;
+
+    m.resize(Dim, Dim);
+    m.setRandom();
+
+    EXPECT_FALSE(fl::is_diagonal(m));
+
+    m = m.diagonal().asDiagonal();
+    EXPECT_TRUE(fl::is_diagonal(m));
+
+    m.setIdentity();
+    m *= 3.;
+    EXPECT_TRUE(fl::is_diagonal(m));
+
+    m(0, Dim - 1) = 2;
+    EXPECT_FALSE(fl::is_diagonal(m));
+}
+
+TEST(LinearAlgebra, is_diagonal_fixed_size)
+{
+    check_is_diagonal<Dim>();
+}
+
+TEST(LinearAlgebra, is_diagonal_dynamic_size)
+{
+    check_is_diagonal<Eigen::Dynamic>();
+}
