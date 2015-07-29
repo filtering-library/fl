cb366184020008bf5b322371e703f334726bd046
issac <jan.issac@gmail.com>
2015-07-29 10:20:30 +0200

    :lipstick:

--- test/gaussian_filter/gaussian_filter_test_suite.hpp
+++ test/gaussian_filter/gaussian_filter_test_suite.hpp
@@ -66,8 +66,8 @@ protected:
     typedef Eigen::Matrix<fl::Real, ObsrvSize, 1> Obsrv;
 
     typedef fl::LinearStateTransitionModel<State, Input> LinearStateTransition;
-    //typedef fl::LinearDecorrelatedGaussianObservationModel<Obsrv, State> LinearObservation;
-    typedef fl::LinearGaussianObservationModel<Obsrv, State> LinearObservation;
+    typedef fl::LinearDecorrelatedGaussianObservationModel<Obsrv, State> LinearObservation;
+    //typedef fl::LinearGaussianObservationModel<Obsrv, State> LinearObservation;
 
 
     typedef typename Configuration::template FilterDefinition<
